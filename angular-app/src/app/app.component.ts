4934611882110c4848db8580995ae345868fadb6
Antonio Manuel <anmamebo2001@gmail.com>
2023-10-30 13:04:51 +0100

    Añadida lógica y estilos tema oscuro

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularFrontend';
+
+  ngOnInit(): void {
+    // Inicialización tema
+    const theme = localStorage.getItem('theme');
+    if (theme) document.documentElement.setAttribute('data-bs-theme', theme);
+  }
 }
--- angular-app/src/app/shared/components/sidebar/sidebar.component.ts
+++ angular-app/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,18 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit {
-  myScriptElement: HTMLScriptElement;
+export class SidebarComponent implements OnInit, AfterViewInit {
+  THEME_KEY: string = 'theme';
+  sidebarScriptElement: HTMLScriptElement;
 
   constructor() {
-    this.myScriptElement = document.createElement('script');
-    this.myScriptElement.src = 'assets/js/components/sidebar.js';
-    document.body.appendChild(this.myScriptElement);
+    this.sidebarScriptElement = document.createElement('script');
+    this.sidebarScriptElement.src = 'assets/js/components/sidebar.js';
+    document.body.appendChild(this.sidebarScriptElement);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initTheme();
+  }
+
+  ngAfterViewInit(): void {
+    const toggler = document.getElementById('toggle-dark') as HTMLInputElement;
+    const theme = localStorage.getItem(this.THEME_KEY);
+
+    if (toggler) {
+      toggler.checked = theme === 'dark';
+
+      toggler.addEventListener('input', (e) => {
+        if (!e.target) return;
+        this.setTheme(
+          (<HTMLInputElement>e.target).checked ? 'dark' : 'light',
+          true
+        );
+      });
+    }
+  }
+
+  private setTheme(theme: string, persist = false) {
+    document.body.classList.add(theme);
+    document.documentElement.setAttribute('data-bs-theme', theme);
+
+    if (persist) {
+      localStorage.setItem(this.THEME_KEY, theme);
+    }
+  }
+
+  /**
+   * Init theme from setTheme()
+   */
+  private initTheme() {
+    //If the user manually set a theme, we'll load that
+    const storedTheme = localStorage.getItem(this.THEME_KEY);
+    if (storedTheme) {
+      return this.setTheme(storedTheme);
+    }
+    //Detect if the user set his preferred color scheme to dark
+    if (!window.matchMedia) {
+      return;
+    }
+
+    //Media query to detect dark preference
+    const mediaQuery = window.matchMedia('(prefers-color-schema: dark)');
+
+    //Register change listener
+    mediaQuery.addEventListener('change', (e) =>
+      this.setTheme(e.matches ? 'dark' : 'light', true)
+    );
+    return this.setTheme(mediaQuery.matches ? 'dark' : 'light', true);
+  }
 }
