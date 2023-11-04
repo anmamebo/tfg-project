31155a5d5ff0c6ca544758fe02ee564a4acc4f6c
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-04 20:37:32 +0100

    Añadida documentación docstrings en vistas de Login y Logout

--- django-app/users/views.py
+++ django-app/users/views.py
@@ -12,12 +12,34 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class Login(TokenObtainPairView):
+  """
+  Vista para iniciar sesión y obtener tokens de acceso.
+  
+  Esta vista permite a los usuarios autenticarse y obtener tokens de acceso y de actualización
+  para su uso posterior en las solicitudes autenticadas.
+
+  Attributes:
+    serializer_class (Serializer): El serializador para representar los datos de inicio de sesión.
+  
+  Permissions:
+    No se requieren permisos específicos para acceder a esta vista.
+  """
+  
   serializer_class = CustomTokenObtainPairSerializer
   
   def post(self, request, *args, **kwargs):
+    """
+    Inicia sesión y obtiene tokens de acceso y de actualización.
+
+    Args:
+        request (Request): La solicitud HTTP que contiene el nombre de usuario y contraseña para la autenticación.
+
+    Returns:
+        Response: La respuesta que contiene los tokens de acceso y de actualización y los datos del usuario o los errores.
+    """
     username = request.data.get('username', '')
     password = request.data.get('password', '')
-    user = authenticate(username=username, password=password)
+    user = authenticate(username=username, password=password) # authenticate retorna el usuario si la autenticación es exitosa, de lo contrario retorna None
     
     if user:
       login_serializer = self.serializer_class(data=request.data)
@@ -29,23 +51,45 @@ class Login(TokenObtainPairView):
           'user': user_serializer.data,
           'message': 'Inicio de sesión exitoso'
         }, status=status.HTTP_200_OK)
+      
       return Response({
         'message': 'Contraseña o nombre de usuario incorrectos',
         'errors': login_serializer.errors
       }, status=status.HTTP_400_BAD_REQUEST)
+    
     return Response({
         'message': 'Contraseña o nombre de usuario incorrectos',
         'errors': login_serializer.errors
       }, status=status.HTTP_400_BAD_REQUEST)
     
 class Logout(GenericAPIView):
+  """
+  Vista para cerrar sesión y revocar tokens de acceso.
+  
+  Esta vista permite a los usuarios cerrar sesión y revocar sus tokens de acceso, lo que impide que se realicen
+  solicitudes autenticadas en su nombre.
+
+  Attributes:
+    No se requieren atributos específicos para esta vista.
+  """
+
   def post(self, request, *args, **kwargs):
-    user = User.objects.filter(id=request.data.get('user', 0))
+    """
+    Cierra sesión y revoca los tokens de acceso.
+
+    Args:
+        request (Request): La solicitud HTTP que contiene el ID del usuario para cerrar sesión.
+
+    Returns:
+        Response: La respuesta que indica sí la sesión se ha cerrado correctamente o si ha habido errores.
+    """
+    user = User.objects.filter(id=request.data.get('user', 0)) # Recupera el usuario por su ID
     if user.exists():
-      RefreshToken.for_user(user.first())
+      RefreshToken.for_user(user.first()) # Revoca los tokens de acceso del usuario
       return Response({
         'message': 'Sesión cerrada correctamente'
       }, status=status.HTTP_200_OK)
+    
     return Response({
         'message': 'No existe este usuario'
       }, status=status.HTTP_400_BAD_REQUEST)
