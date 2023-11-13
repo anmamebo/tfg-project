4f950bcbf6d0c4fc7e36db6b031675428332309d
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-13 19:01:51 +0100

    Backend - Actualizado serializer Patient

--- django-app/patients/api/serializers/patient_serializer.py
+++ django-app/patients/api/serializers/patient_serializer.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
 
-# from users.api.serializers.user_serializer import UserSerializer
 from patients.api.serializers.address_serializer import AddressSerializer
 from patients.models import Patient
 
 
 class PatientSerializer(serializers.ModelSerializer):
-  # user = UserSerializer(read_only=True)
+  user = serializers.SerializerMethodField(read_only=True)
   address = AddressSerializer(read_only=True)
   
   class Meta:
     model = Patient
     exclude = ['state', 'created_date', 'modified_date', 'deleted_date']
+    
+  def get_user(self, obj):
+    user = obj.user
+    return {
+      'id': user.id,
+      'username': user.username,
+      'email': user.email,
+      'name': user.name,
+      'last_name': user.last_name,
+    }
