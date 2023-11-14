df91b0fffd02c77a12acafd4c33bda18f32eed17
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-14 16:27:09 +0100

    Backend - Implementada búsqueda en viewset de Permission

--- django-app/users/api/viewsets/permission_viewsets.py
+++ django-app/users/api/viewsets/permission_viewsets.py
@@ -1,8 +1,8 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import Permission
 
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.response import Response
 
 from users.api.serializers.permission_serializer import PermissionSerializer
@@ -24,7 +24,9 @@ class PermissionViewSet(viewsets.GenericViewSet):
   model = Permission
   serializer_class = PermissionSerializer
   list_serializer_class = PermissionSerializer
-  queryset = None
+  search_fields = ['name', 'codename', 'content_type__model']
+  filter_backends = [filters.SearchFilter]
+  queryset = Permission.objects.all()
   
   def get_object(self, pk):
     return get_object_or_404(self.model, pk=pk)
@@ -46,6 +48,17 @@ class PermissionViewSet(viewsets.GenericViewSet):
         Response: La respuesta que contiene la lista de permisos.
     """
     permissions = self.get_queryset()
+    
+    # Obtener el parámetro de búsqueda de la solicitud
+    query = self.request.query_params.get('search', None)
+    
+    # Realizar la búsqueda si se proporciona un parámetro de búsqueda
+    if query:
+        # Buscar en múltiples campos utilizando la cláusula Q de Django
+        permissions = permissions.filter(
+            Q(name__icontains=query) | Q(codename__icontains=query) | Q(content_type__model__icontains=query)
+        )
+    
     page = self.paginate_queryset(permissions)
     if page is not None:
       permissions_serializer = self.list_serializer_class(page, many=True)
