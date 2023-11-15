95c3feff76a11c971e24d6533e6dd56ef7a456ab
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-15 16:35:00 +0100

    Frontend - Refactorizado auth guard

--- angular-app/src/app/core/guards/auth.guard.spec.ts
+++ angular-app/src/app/core/guards/auth.guard.spec.ts
@@ -1,19 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { CanActivateFn } from '@angular/router';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { AuthGuard } from './auth.guard';
+import { authGuard } from './auth.guard';
 
-describe('AuthGuard', () => {
-  let guard: AuthGuard;
+describe('authGuard', () => {
+  const executeGuard: CanActivateFn = (...guardParameters) =>
+    TestBed.runInInjectionContext(() => authGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
     });
-    guard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
-    expect(guard).toBeTruthy();
+    expect(executeGuard).toBeTruthy();
   });
 });
--- angular-app/src/app/core/guards/auth.guard.ts
+++ angular-app/src/app/core/guards/auth.guard.ts
@@ -1,20 +1,17 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 import { AuthService } from '../services/auth.service';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (!this.authService.isLogin()) {
-      this.router.navigate(['/auth/login']);
-      return false;
-    }
-    return true;
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (!authService.isLogin()) {
+    router.navigate(['/auth/login']);
+    return false;
   }
-}
+
+  return true;
+};
