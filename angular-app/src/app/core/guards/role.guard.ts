c4b8b6b11d3f687a06621b605aa3d49ca51e46ee
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-15 16:00:44 +0100

    Frontend - Creado Guard para los roles

---
+++ angular-app/src/app/core/guards/role.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from "../services/auth.service";
+import { inject } from '@angular/core';
+
+
+export const roleGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const rolesRequired = route.data['roles'] as string[];
+
+  if (rolesRequired && rolesRequired.length >  0) {
+    const userRoles = authService.getRolesFromToken();
+    const hasRequiredRole = rolesRequired.some((role) => userRoles.includes(role));
+  
+    if (hasRequiredRole) {
+      return true;
+    } else {
+      router.navigate(['**']);
+      return false;
+    }
+  }
+
+  return true;
+};
