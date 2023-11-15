e8fd95746f6cd9f9ca322f3e35514ec5367e9425
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-15 16:01:08 +0100

    Frontend - Creada directiva personalizada para comprobar roles

---
+++ angular-app/src/app/core/directives/has-role.directive.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HasRoleDirective } from './has-role.directive';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { TemplateRef, ViewContainerRef } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+
+describe('HasRoleDirective', () => {
+  it('should create an instance', () => {
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        AuthService,
+        TemplateRef,
+        ViewContainerRef
+      ]
+    });
+
+    // Crea una instancia de la directiva
+    const directive = new HasRoleDirective(
+      TestBed.inject(AuthService),
+      TestBed.inject(TemplateRef),
+      TestBed.inject(ViewContainerRef)
+    );
+    
+    // Verifica si la instancia fue creada correctamente
+    expect(directive).toBeTruthy();
+
+
+    // const directive = new HasRoleDirective();
+    // expect(directive).toBeTruthy();
+  });
+});
