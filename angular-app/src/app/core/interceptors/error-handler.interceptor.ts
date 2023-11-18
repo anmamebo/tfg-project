29de62fade9c751915daa1c2f868e60c06ed6128
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-18 15:17:21 +0100

    Frontend - Añadido al interceptor error-handler el mostrado de mensajes de validación

--- angular-app/src/app/core/interceptors/error-handler.interceptor.ts
+++ angular-app/src/app/core/interceptors/error-handler.interceptor.ts
@@ -11,13 +11,25 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
         let errorMessage = 'Ocurrió un error en el servidor. Inténtalo más tarde.';
-        
+
         if (error.status === 400) {
           errorMessage = error.error.message || 'Hubo un problema con la solicitud.';
         } else if (error.status === 404) {
           errorMessage = error.error.message || 'No se encontró el recurso solicitado.';
         } else if (error.status === 0) {
           errorMessage = 'Erro de conexión. Comprueba tu conexión a internet.';
+        }
+
+        // Si el error tiene errores de validación
+        if (error.error.errors) {
+          errorMessage += "\n";
+          const errors: any[] = error.error.errors;
+          Object.keys(errors).forEach((key: any) => {
+            const messages: any[] = errors[key];
+            messages.forEach((message: any) => {
+              errorMessage += message + "\n";
+            });
+          });
         }
 
         throw new Error(errorMessage);
