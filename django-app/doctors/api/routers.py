1e6b8da740194e2ad2a898a77ddc09d33a05ce9d
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-22 20:04:43 +0100

    Backend - Creado viewset para medicalspecialty con método para listar las especialidades médicas

--- django-app/doctors/api/routers.py
+++ django-app/doctors/api/routers.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
 from doctors.api.viewsets.doctor_viewsets import DoctorViewSet
+from doctors.api.viewsets.medicalspecialty_viewsets import MedicalSpecialtyViewSet
 
 router = DefaultRouter()
 
 router.register(r'doctors', DoctorViewSet, basename="doctors")
+router.register(r'medicalspecialties', MedicalSpecialtyViewSet, basename="medicalspecialties")
 
 urlpatterns = router.urls
