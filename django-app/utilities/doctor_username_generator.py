090d4c8f21e8ee6e90785480d3e7d8692bed99eb
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-22 13:49:59 +0100

    Backend - Creada función para generar el nombre de usuario de un médico

---
+++ django-app/utilities/doctor_username_generator.py
@@ -0,0 +1,24 @@
+from users.models import User
+from unidecode import unidecode
+
+
+def generate_doctor_username(name, last_name):
+  # Quita los acentos y caracteres especiales
+  name = unidecode(name)
+  last_name = unidecode(last_name)
+
+  # Obtiene las iniciales del nombre y apellido 
+  names = name.split()
+  initial_name = ''.join([n[0].lower() for n in names])
+  
+  # Genera el nombre de usuario
+  base_username = f"{initial_name}{last_name.lower()}"
+  username = base_username[:30]
+  
+  # Añade un número al final del nombre de usuario si ya existe
+  counter = 1
+  while User.objects.filter(username=username).exists():
+    username = f"{base_username[:28]}{counter}"
+    counter += 1
+    
+  return username
