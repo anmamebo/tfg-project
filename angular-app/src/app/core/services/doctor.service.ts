78c0f2b5f21abda067b3d662c8adff14876a7ac9
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-23 12:41:48 +0100

    Frontend - Añadidos métodos CRUD a servicio de Doctor

--- angular-app/src/app/core/services/doctor.service.ts
+++ angular-app/src/app/core/services/doctor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Observable, catchError } from 'rxjs';
 
 import { API_URL } from '../constants/API_URL';
@@ -29,6 +29,54 @@ export class DoctorService {
   }
 
   /**
+   * Obtiene un médico por su identificador.
+   * @param doctor El identificador del médico.
+   * @returns Un observable que emite un objeto `Doctor`.
+   */
+  public getDoctorById(id: number): Observable<Doctor> {
+    const headers = this.httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    return this.http.get<Doctor>(this.url + id + '/', httpOptions);
+  }
+
+  /**
+   * Obtiene todos los médicos.
+   * @param page El número de página que se quiere obtener.
+   * @param searchTerm Término de búsqueda.
+   * @returns Un observable que emite un objeto `any`.
+   */
+  public getDoctors(page: number, pageSize: number, searchTerm: string): Observable<any> {
+    const headers = this.httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    // Parámetros de la petición
+    let params = new HttpParams().set('page', page.toString());
+    params = params.set('page_size', pageSize.toString());
+
+    // Comprueba si se ha indicado un término de búsqueda
+    if (searchTerm) {
+      params = params.set('search', searchTerm);
+    }
+
+    return this.http.get<any>(this.url, { params, ...httpOptions });
+  }
+
+  /**
+   * Crea un nuevo médico.
+   * @param doctor El objeto `Doctor` con los datos del nuevo médico.
+   * @returns Un observable que emite un objeto `any`.
+   */
+  public createDoctor(doctor: Doctor): Observable<any> {
+    const headers = this.httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    let params = JSON.stringify(doctor);
+
+    return this.http.post<any>(this.url, params, httpOptions);
+  }
+
+  /**
    * Actualiza los datos de un médico.
    * @param doctor El objeto `Doctor` con los datos actualizados.
    * @returns Un observable que emite un objeto `any`.
@@ -40,5 +88,29 @@ export class DoctorService {
     let params = JSON.stringify(doctor);
 
     return this.http.put<any>(this.url + doctor.id + '/', params, httpOptions);
+  }
+
+  /**
+   * Elimina un médico.
+   * @param id El identificador del médico.
+   * @returns Un observable que emite un objeto `any`.
+   */
+  public deleteDoctor(id: string): Observable<any> {
+    const headers = this.httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    return this.http.delete<any>(this.url + id + '/', httpOptions);
+  }
+
+  /**
+   * Activa un médico.
+   * @param id El identificador del médico.
+   * @returns Un observable que emite un objeto `any`.
+   */
+  public activateDoctor(id: string): Observable<any> {
+    const headers = this.httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    return this.http.put<any>(this.url + id + '/activate/', {}, httpOptions);
   }
 }
