b74ee69277c5d0d89ae824d85d17d5f9260593f5
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-23 12:42:17 +0100

    Frontend - Creado resolver para Doctor

---
+++ angular-app/src/app/core/resolvers/doctor.resolver.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { ResolveFn } from '@angular/router';
+
+import { doctorResolver } from './doctor.resolver';
+
+describe('doctorResolver', () => {
+  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
+      TestBed.runInInjectionContext(() => doctorResolver(...resolverParameters));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(executeResolver).toBeTruthy();
+  });
+});
---
+++ angular-app/src/app/core/resolvers/doctor.resolver.ts
@@ -0,0 +1,32 @@
+import { ActivatedRouteSnapshot, ResolveFn, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable, catchError, map, of } from 'rxjs';
+import { inject } from '@angular/core';
+
+import { DoctorService } from "../services/doctor.service";
+
+
+export const doctorResolver: ResolveFn<any> = (
+  route: ActivatedRouteSnapshot, 
+  state: RouterStateSnapshot,
+  doctorService: DoctorService = inject(DoctorService),
+  router: Router = inject(Router)
+): Observable<any> => {
+  
+  const doctorId = route.params['id'];
+  return doctorService.getDoctorById(doctorId)
+    .pipe(
+      map(doctor => {
+        if (doctor) {
+          return doctor;
+        } else {
+          router.navigate(['**'], { replaceUrl: true });
+          return null; 
+        }
+      }),
+      catchError(() => {
+        router.navigate(['**'], { replaceUrl: true }); // Redirecciona a la página de error cuando la solicitud falla
+        return of(null);
+      })
+    );
+
+};
