4dc91a20e2140ee7e770019a045decc88c6af3fb
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-24 13:05:19 +0100

    Backend - Implementado viewset completo para Department

--- django-app/departments/api/serializers/department_serializer.py
+++ django-app/departments/api/serializers/department_serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from departments.models import Department
+from departments.models import Department, Room
 from doctors.api.serializers.doctor_serializer import DoctorInDepartmentListSerializer
 
 
@@ -9,8 +9,14 @@ class DepartmentSerializer(serializers.ModelSerializer):
     model = Department
     exclude = ['state', 'created_date', 'modified_date', 'deleted_date']
 
+class RoomForDepartmentSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Room
+    fields = ['id', 'name', 'type', 'location', 'capacity']
+
 class DepartmentWithDoctorsSerializer(serializers.ModelSerializer):
   doctor_set = DoctorInDepartmentListSerializer(read_only=True, many=True)
+  room_set = RoomForDepartmentSerializer(read_only=True, many=True)
   class Meta:
     model = Department
     exclude = ['state', 'created_date', 'modified_date', 'deleted_date']
--- django-app/departments/api/viewsets/department_viewsets.py
+++ django-app/departments/api/viewsets/department_viewsets.py
@@ -1,8 +1,9 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 
 from departments.models import Department
 from departments.api.serializers.department_serializer import (DepartmentSerializer, DepartmentWithDoctorsSerializer)
@@ -47,5 +48,137 @@ class DepartmentViewSet(viewsets.GenericViewSet):
         Response: La respuesta que contiene la lista de departamentos.
     """
     departments = self.get_queryset()
+    departments_serializer = self.list_serializer_class(departments, many=True)
+    return Response(departments_serializer.data, status=status.HTTP_200_OK)
+  
+  def create(self, request):
+    """
+    Crea un departamento.
+
+    Args:
+        request (Request): La solicitud HTTP.
+
+    Returns:
+        Response: La respuesta el resultado de la operación.
+    """
+    department_serializer = self.serializer_class(data=request.data)
+    if department_serializer.is_valid():
+      department_serializer.save()
+      return Response({
+        'message': 'Departamento creado correctamente.'
+      }, status=status.HTTP_201_CREATED)
+    
+    return Response(department_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  def retrieve(self, request, pk=None):
+    """
+    Obtiene un departamento.
+
+    Args:
+        request (Request): La solicitud HTTP.
+        pk (int): El id del departamento a obtener.
+
+    Returns:
+        Response: La respuesta que contiene el departamento.
+    """
+    department = self.get_object(pk)
+    department_serializer = self.serializer_class(department)
+    return Response(department_serializer.data, status=status.HTTP_200_OK)
+  
+  def update(self, request, pk=None):
+    """
+    Actualiza un departamento.
+
+    Args:
+        request (Request): La solicitud HTTP.
+        pk (int): El id del departamento a actualizar.
+
+    Returns:
+        Response: La respuesta el resultado de la operación.
+    """
+    department = self.get_object(pk)
+    department_serializer = self.serializer_class(department, data=request.data)
+    if department_serializer.is_valid():
+      department_serializer.save()
+      return Response({
+        'message': 'Departamento actualizado correctamente.'
+      }, status=status.HTTP_200_OK)
+    
+    return Response({
+      'message': 'Hay errores en la actualización.',
+      'errors': department_serializer.errors
+    }, status=status.HTTP_400_BAD_REQUEST)  
+    
+  def destroy(self, request, pk=None):
+    """
+    Elimina un departamento.
+
+    Args:
+        request (Request): La solicitud HTTP.
+        pk (int): El id del departamento a eliminar.
+
+    Returns:
+        Response: La respuesta el resultado de la operación.
+    """
+    department = self.get_queryset().filter(id=pk).first()
+    if department:
+      department.state = False
+      department.save()
+      return Response({
+        'message': 'Departamento eliminado correctamente.'
+      }, status=status.HTTP_200_OK)
+      
+    return Response({
+      'message': 'No se ha encontrado el departamento.'
+    }, status=status.HTTP_400_BAD_REQUEST)
+    
+  @action(detail=True, methods=['put'])
+  def activate(self, request, pk=None):
+    """
+    Activa un departamento.
+
+    Args:
+        request (Request): La solicitud HTTP.
+        pk (int): El id del departamento a activar.
+
+    Returns:
+        Response: La respuesta el resultado de la operación.
+    """
+    department = self.get_object(pk)
+    if department:
+      department.state = True
+      department.save()
+      return Response({
+        'message': 'Departamento activado correctamente.'
+      }, status=status.HTTP_200_OK)
+      
+    return Response({
+      'message': 'No se ha encontrado el departamento.'
+    }, status=status.HTTP_400_BAD_REQUEST)
+    
+  @action(detail=False, methods=['get'])
+  def list_paginate(self, request):
+    """
+    Lista los departamentos paginados.
+
+    Args:
+        request (Request): La solicitud HTTP.
+
+    Returns:
+        Response: La respuesta que contiene la lista de departamentos paginados.
+    """
+    departments = self.get_queryset()
+    
+    query = self.request.query_params.get('search', None)
+    if query:
+      departments = departments.filter(
+        Q(name__icontains=query) | Q(description__icontains=query)
+      )
+    
+    page = self.paginate_queryset(departments)
+    if page is not None:
+      departments_serializer = self.list_serializer_class(page, many=True)
+      return self.get_paginated_response(departments_serializer.data)
+    
     departments_serializer = self.list_serializer_class(departments, many=True)
     return Response(departments_serializer.data, status=status.HTTP_200_OK)
