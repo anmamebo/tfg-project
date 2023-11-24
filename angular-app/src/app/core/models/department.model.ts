1777b3b398fdd731774aa8d9e32a90e5655e6dd5
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-24 17:49:50 +0100

    Frontend - Actualizado modelo Department

--- angular-app/src/app/core/models/department.model.ts
+++ angular-app/src/app/core/models/department.model.ts
@@ -1,10 +1,12 @@
 import { Doctor } from "./doctor.model";
+import { Room } from "./room.model";
 
 export class Department {
   constructor(
     public id: string,
     public name: string,
     public description?: string,
-    public doctors?: Doctor[],
+    public doctor_set?: Doctor[],
+    public room_set?: Room[],
   ) {}
 }
