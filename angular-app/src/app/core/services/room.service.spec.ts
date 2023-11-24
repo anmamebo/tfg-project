c82ef48ed66d795438d1e44f36ec1571494136ca
Antonio Manuel <anmamebo2001@gmail.com>
2023-11-24 18:00:57 +0100

    Frontend - Creado servicio para Room, con método para obtener salas de un departamento

---
+++ angular-app/src/app/core/services/room.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { RoomService } from './room.service';
+
+describe('RoomService', () => {
+  let service: RoomService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(RoomService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ angular-app/src/app/core/services/room.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+import { API_URL } from '../constants/API_URL';
+
+// Servicios
+import { HttpCommonService } from "./http-common.service";
+
+// Modelos
+import { Room } from "../models/room.model";
+
+
+/**
+ * Servicio para interactuar con la API para la gestión de salas.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+  /** URL base de la API. */
+  public url: string;
+
+  constructor(
+    private http: HttpClient,
+    private httpCommonService: HttpCommonService,
+  ) {
+    this.url = API_URL.url + 'departments/rooms/';
+  }
+
+  /**
+   * Obtiene listado de salas de un departamento.
+   * @param id ID del departamento.
+   * @param page página actual.
+   * @param pageSize tamaño de la página.
+   * @returns Un observable que emite un objeto `any`.
+   */
+  public getRoomsByDepartmentId(id: number, page: number, pageSize:number): Observable<any> {
+    const headers = this.httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    let params = new HttpParams().set('department', id.toString());
+    params = params.set('page', page.toString());
+    params = params.set('page_size', pageSize.toString());
+    params = params.set('paginate', 'true');    
+
+    return this.http.get<any>(this.url + 'rooms_by_department', { params, ...httpOptions });
+  }
+}
