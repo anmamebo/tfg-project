acd3624044b416b9245ddaffbfc2b812c2b1464a
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-01 19:36:02 +0100

    Frontend - Creadas constantes para opciones de Appointments

---
+++ angular-app/src/app/core/constants/options/status-appointment-options.constants.ts
@@ -0,0 +1,9 @@
+export const STATUS_APPOINTMENT_OPTIONS = [
+  { value: 'pending', text: 'Pendiente' },
+  { value: 'scheduled', text: 'Programada' },
+  { value: 'in_progress', text: 'En progreso' },
+  { value: 'completed', text: 'Completada' },
+  { value: 'rescheduled', text: 'Reprogramada' },
+  { value: 'no_show', text: 'No presentado' },
+  { value: 'cancelled', text: 'Cancelada'},
+];
---
+++ angular-app/src/app/core/constants/options/type-appointment-options.constants.ts
@@ -0,0 +1,7 @@
+export const TYPE_APPOINTMENT_OPTIONS = [
+  { value: 'consultation', text: 'Consulta' },
+  { value: 'procedure', text: 'Procedimiento' },
+  { value: 'checkup', text: 'Revisión' },
+  { value: 'surgery', text: 'Cirugía' },
+  { value: 'other', text: 'Otro' },
+];
