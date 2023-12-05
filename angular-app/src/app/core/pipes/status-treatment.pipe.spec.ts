87e33880210de9aca771576943626bb59f2a7f7a
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-05 14:41:25 +0100

    Frontend - Creado pipe para estados de tratamiento

---
+++ angular-app/src/app/core/pipes/status-treatment.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { StatusTreatmentPipe } from './status-treatment.pipe';
+
+describe('StatusTreatmentPipe', () => {
+  it('create an instance', () => {
+    const pipe = new StatusTreatmentPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ angular-app/src/app/core/pipes/status-treatment.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { STATUS_TREATMENT_OPTIONS } from "src/app/core/constants/options/status-treatment-options.constants";
+
+@Pipe({
+  name: 'statusTreatment'
+})
+export class StatusTreatmentPipe implements PipeTransform {
+
+  transform(status: string | null | undefined): string {
+    const statusOption = STATUS_TREATMENT_OPTIONS.find(option => option.value === status);
+    
+    return statusOption ? statusOption.text : 'Otro';
+  }
+
+}
