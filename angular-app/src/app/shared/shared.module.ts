5203badc36f3e4b99a25fbda4cd2950ac2facf63
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-05 16:56:46 +0100

    Fontend - Añadidos pipes a shared module

--- angular-app/src/app/shared/shared.module.ts
+++ angular-app/src/app/shared/shared.module.ts
@@ -1,21 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Directives
 import { HasRoleDirective } from "src/app/core/directives/has-role.directive";
+
+// Pipes
 import { GenderPipe } from 'src/app/core/pipes/gender.pipe';
+import { TypeAppointmentPipe } from 'src/app/core/pipes/type-appointment.pipe';
+import { StatusAppointmentPipe } from "src/app/core/pipes/status-appointment.pipe";
+import { StatusTreatmentPipe } from "src/app/core/pipes/status-treatment.pipe";
 
 
 @NgModule({
   declarations: [
     HasRoleDirective,
     GenderPipe,
+    TypeAppointmentPipe,
+    StatusAppointmentPipe,
+    StatusTreatmentPipe,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HasRoleDirective, 
-    GenderPipe
+    GenderPipe,
+    TypeAppointmentPipe,
+    StatusAppointmentPipe,
+    StatusTreatmentPipe,
   ]
 })
 export class SharedModule { }
