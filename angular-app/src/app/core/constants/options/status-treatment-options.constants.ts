ad703b404b9fdcbfe00258ee72612396d7d3c0f2
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-05 14:40:48 +0100

    Frontend - Creadas opciones de estado de tratamiento

---
+++ angular-app/src/app/core/constants/options/status-treatment-options.constants.ts
@@ -0,0 +1,6 @@
+export const STATUS_TREATMENT_OPTIONS = [
+  { value: 'in_progress', text: 'En progreso' },
+  { value: 'completed', text: 'Completado' },
+  { value: 'interrupted', text: 'Interrumpido' },
+  { value: 'cancelled', text: 'Cancelado' },
+];
