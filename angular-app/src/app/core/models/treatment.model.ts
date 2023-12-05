b345a9b08f458697e8eee115a4896bbde4ae01ba
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-05 14:41:05 +0100

    Frontend - Actualizado modelo Treatment

--- angular-app/src/app/core/models/treatment.model.ts
+++ angular-app/src/app/core/models/treatment.model.ts
@@ -2,13 +2,24 @@ import { Patient } from "./patient.model";
 import { Doctor } from "./doctor.model";
 import { Appointment } from "./appointment.model";
 
+export interface StatusBadgeClasses {
+  [status: string]: string;
+}
+
+export const STATUS_BADGE_CLASSES: StatusBadgeClasses = {
+  in_progress: 'bg-warning',
+  completed: 'bg-success',
+  interrupted: 'bg-dark',
+  cancelled: 'bg-danger',
+};
+
 export class Treatment {
   constructor(
     public id: string,
     public status: string,
-    public description?: string,
-    public duration? : string,
-    public start_date?: Date,
+    public description: string,
+    public duration : string,
+    public start_date: Date,
     public end_date?: Date,
     public comments?: string,
     public application_frequency?: string,
