db63014ac8590edca054811fff3f134d42acaa05
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-06 15:12:41 +0100

    Frontend - Actualizado error-handler para posibilidades de errores anidados

--- angular-app/src/app/core/interceptors/error-handler.interceptor.ts
+++ angular-app/src/app/core/interceptors/error-handler.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable()
 export class ErrorHandlerInterceptor implements HttpInterceptor {
@@ -11,7 +11,7 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
         let errorMessage = 'Ocurrió un error en el servidor. Inténtalo más tarde.';
-
+        
         if (error.status === 400) {
           errorMessage = error.error.message || 'Hubo un problema con la solicitud.';
         } else if (error.status === 404) {
@@ -20,16 +20,26 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
           errorMessage = 'Erro de conexión. Comprueba tu conexión a internet.';
         }
 
-        // Si el error tiene errores de validación
-        if (error.error.errors) {
-          errorMessage += "\n";
-          const errors: any[] = error.error.errors;
-          Object.keys(errors).forEach((key: any) => {
-            const messages: any[] = errors[key];
-            messages.forEach((message: any) => {
-              errorMessage += message + "\n";
+        // Manejo de errores anidados
+        const handleNestedErrors = (errors: any) => {
+          if (typeof errors === 'object') { // Si es un objeto
+            Object.keys(errors).forEach((key: any) => { // Recorremos las llaves
+              const errorValue = errors[key]; // Obtenemos el valor del error
+              
+              if (Array.isArray(errorValue)) { // Si es un arreglo
+                errorValue.forEach((message: any) => { // Recorremos los mensajes
+                  errorMessage += '\n' + message; 
+                });
+              } else if (typeof errorValue === 'object') { // Si es un objeto
+                handleNestedErrors(errorValue); // Volvemos a llamar a la función
+              }
+
             });
-          });
+          }
+        };
+
+        if (error.error.errors) {
+          handleNestedErrors(error.error.errors);
         }
 
         throw new Error(errorMessage);
