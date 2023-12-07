9ee1556ff1e01d3ce29b98e8b038078319172b4e
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-07 21:56:06 +0100

    Frontend - Refactorizado schedule-page.component.ts

--- angular-app/src/app/modules/schedule/pages/schedule-page/schedule-page.component.ts
+++ angular-app/src/app/modules/schedule/pages/schedule-page/schedule-page.component.ts
@@ -14,27 +14,11 @@ import {
   CalendarEventTimesChangedEvent,
   CalendarView,
 } from 'angular-calendar';
-import { EventColor } from 'calendar-utils';
 
 import { breadcrumbScheduleData } from 'src/app/core/constants/breadcrumb-data';
 
 // Servicios
 import { ScheduleService } from 'src/app/core/services/schedule.service';
-
-const colors: Record<string, EventColor> = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3',
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF',
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA',
-  },
-};
 
 /**
  * Componente que representa la página de mi horario
@@ -71,48 +55,7 @@ export class SchedulePageComponent implements OnInit {
   /** Lista de eventos del calendario. */
   events: CalendarEvent[] = [];
 
-  // events: CalendarEvent[] = [
-  //   {
-  //     start: subDays(startOfDay(new Date()), 1),
-  //     end: addDays(new Date(), 1),
-  //     title: 'A 3 day event',
-  //     color: { ...colors['red'] },
-  //     actions: this.actions,
-  //     allDay: true,
-  //     resizable: {
-  //       beforeStart: true,
-  //       afterEnd: true,
-  //     },
-  //     draggable: true,
-  //   },
-  //   {
-  //     start: startOfDay(new Date()),
-  //     title: 'An event with no end date',
-  //     color: { ...colors['yellow'] },
-  //     actions: this.actions,
-  //   },
-  //   {
-  //     start: subDays(endOfMonth(new Date()), 3),
-  //     end: addDays(endOfMonth(new Date()), 3),
-  //     title: 'A long event that spans 2 months',
-  //     color: { ...colors['blue'] },
-  //     allDay: true,
-  //   },
-  //   {
-  //     start: addHours(startOfDay(new Date()), 2),
-  //     end: addHours(new Date(), 2),
-  //     title: 'A draggable and resizable event',
-  //     color: { ...colors['yellow'] },
-  //     actions: this.actions,
-  //     resizable: {
-  //       beforeStart: true,
-  //       afterEnd: true,
-  //     },
-  //     draggable: true,
-  //   },
-  // ];
-
-  /** Variable que indica si un día está abierto en la vista mensual del calendario. */
+  /** Indica si un día está abierto en la vista mensual del calendario. */
   activeDayIsOpen: boolean = false;
 
   constructor(
@@ -138,7 +81,6 @@ export class SchedulePageComponent implements OnInit {
             this.datePipe.transform(schedule.start_time, 'h:mm a') +
             ' - ' +
             this.datePipe.transform(schedule.end_time, 'h:mm a'),
-          color: { ...colors['blue'] },
         }));
         this.cdr.detectChanges();
       },
