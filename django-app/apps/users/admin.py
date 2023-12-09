1cb51698763327746d53391e8e6fe6f10db9a423
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-09 21:45:23 +0100

    Backend - Actualizado panel django admin y nombre app de Users

--- django-app/apps/users/admin.py
+++ django-app/apps/users/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
 from apps.users.models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "username",
+        "name",
+        "last_name",
+        "is_active",
+    )
+    readonly_fields = ("id",)
+    search_fields = (
+        "email",
+        "username",
+        "name",
+        "last_name",
+    )
+    list_filter = (
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+    list_per_page = 10
+
+
+admin.site.register(User, UserAdmin)
