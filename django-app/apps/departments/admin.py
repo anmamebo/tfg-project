874e5a69588e39b4d4faad493049d60c4cfd5bab
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-09 21:06:44 +0100

    Backend - Actualizado panel django admin y nombre app de Departments

--- django-app/apps/departments/admin.py
+++ django-app/apps/departments/admin.py
@@ -3,5 +3,32 @@ from django.contrib import admin
 from apps.departments.models import Department, Room
 
 
-admin.site.register(Department)
-admin.site.register(Room)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "created_date",
+    )
+    readonly_fields = ("id", "created_date", "modified_date")
+    search_fields = ("name", "description")
+    ordering = ("-created_date",)
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "type",
+        "location",
+        "capacity",
+        "is_available",
+        "department",
+    )
+    readonly_fields = ("id", "created_date", "modified_date")
+    search_fields = ("name", "description", "type", "location", "capacity")
+    list_filter = ("is_available", "department")
+    ordering = ("-created_date",)
+
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Room, RoomAdmin)
--- django-app/apps/departments/apps.py
+++ django-app/apps/departments/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DepartmentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.departments"
+    verbose_name = "Departamentos"
