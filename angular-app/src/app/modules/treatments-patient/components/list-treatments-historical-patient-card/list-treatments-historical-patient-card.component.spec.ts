735ff8f8274d2c72e4cebbacf72fac60e53a33fa
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-13 20:43:02 +0100

    Frontend - Arreglado test componente listado de tratamientos historico

--- angular-app/src/app/modules/treatments-patient/components/list-treatments-historical-patient-card/list-treatments-historical-patient-card.component.spec.ts
+++ angular-app/src/app/modules/treatments-patient/components/list-treatments-historical-patient-card/list-treatments-historical-patient-card.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ListTreatmentsHistoricalPatientCardComponent } from './list-treatments-historical-patient-card.component';
 
@@ -8,9 +9,12 @@ describe('ListTreatmentsHistoricalPatientCardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ListTreatmentsHistoricalPatientCardComponent]
+      declarations: [ListTreatmentsHistoricalPatientCardComponent],
+      imports: [HttpClientTestingModule],
     });
-    fixture = TestBed.createComponent(ListTreatmentsHistoricalPatientCardComponent);
+    fixture = TestBed.createComponent(
+      ListTreatmentsHistoricalPatientCardComponent
+    );
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
