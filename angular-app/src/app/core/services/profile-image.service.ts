194a598e9a35910b74d8010e487bb412e85ea44e
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-13 20:41:06 +0100

    Frontend - Creado servicio para actualizar foto de perfil cuando cambia

---
+++ angular-app/src/app/core/services/profile-image.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, EventEmitter } from '@angular/core';
+
+/**
+ * Servicio que gestiona la imagen de perfil.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileImageService {
+  /** Evento que se emite cuando se actualiza la imagen de perfil. */
+  public profileImageUpdated: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor() {}
+
+  /**
+   * Emite el evento de actualización de imagen de perfil.
+   * @param imageUrl URL de la imagen de perfil.
+   */
+  public emitProfileImageUpdated(imageUrl: string) {
+    this.profileImageUpdated.emit(imageUrl);
+  }
+}
