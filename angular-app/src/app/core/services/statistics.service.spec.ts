51ff954284b6e04acbaaad6378b06a832b312f94
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-15 00:29:53 +0100

    Frontend - Creado servicio para obtener estadísticas

---
+++ angular-app/src/app/core/services/statistics.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { StatisticsService } from './statistics.service';
+
+describe('StatisticsService', () => {
+  let service: StatisticsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(StatisticsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ angular-app/src/app/core/services/statistics.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { API_URL } from '../constants/API_URL';
+
+// Servicios
+import { HttpCommonService } from './http-common.service';
+
+/**
+ * Servicio para interactuar con la API para la gestión de estadísticas.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class StatisticsService {
+  /** URL base de la API. */
+  public url: string;
+
+  constructor(
+    private _http: HttpClient,
+    private _httpCommonService: HttpCommonService
+  ) {
+    this.url = API_URL.url + 'statistics/';
+  }
+
+  /**
+   * Obtiene las estadísticas generales.
+   * @returns Observable con las estadísticas generales de la aplicación.
+   */
+  public getOverallStats(): Observable<any> {
+    const headers = this._httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    return this._http.get(this.url + 'get_overall_stats/', httpOptions);
+  }
+
+  /**
+   * Obtiene las estadísticas de citas por día.
+   * @param month mes
+   * @param year año
+   * @returns Observable con las estadísticas de citas por día.
+   */
+  public getAppointmentsPerDay(month: number, year: number): Observable<any> {
+    const headers = this._httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    let params = new HttpParams();
+    params = params.append('month', month.toString());
+    params = params.append('year', year.toString());
+
+    return this._http.get(this.url + 'get_appointments_per_day/', {
+      params,
+      ...httpOptions,
+    });
+  }
+}
