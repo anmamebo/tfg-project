038abded8d474808332cbe1e4c7671b332b8be9e
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-15 20:53:53 +0100

    Backend - Creado serializador para estadisticas de las especialidades médicas

--- django-app/apps/doctors/api/serializers/medicalspecialty_serializer.py
+++ django-app/apps/doctors/api/serializers/medicalspecialty_serializer.py
@@ -6,3 +6,11 @@ class MedicalSpecialtySerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicalSpecialty
         exclude = ["state", "created_date", "modified_date", "deleted_date"]
+
+
+class MedicalSpecialtyStatisticsSerializer(serializers.ModelSerializer):
+    doctor_count = serializers.IntegerField(source="doctor_set.count")
+
+    class Meta:
+        model = MedicalSpecialty
+        fields = ["name", "doctor_count"]
