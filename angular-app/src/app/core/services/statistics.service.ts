242ae6c993d8860bd37e2bb527e8f8f7a41bbf83
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-15 20:54:50 +0100

    Frontend - Creados métodos en el servicio de estadísticas

--- angular-app/src/app/core/services/statistics.service.ts
+++ angular-app/src/app/core/services/statistics.service.ts
@@ -56,6 +56,29 @@ export class StatisticsService {
   }
 
   /**
+   * Obtiene las estadísticas de citas por día y por género.
+   * @param month mes
+   * @param year año
+   * @returns Observable con las estadísticas de citas por día.
+   */
+  public getAppointmentsPerDayAndGender(
+    month: number,
+    year: number
+  ): Observable<any> {
+    const headers = this._httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    let params = new HttpParams();
+    params = params.append('month', month.toString());
+    params = params.append('year', year.toString());
+
+    return this._http.get(this.url + 'get_appointments_per_day_and_gender/', {
+      params,
+      ...httpOptions,
+    });
+  }
+
+  /**
    * Obtiene las estadísticas de citas por estado.
    * @returns Observable con las estadísticas de citas por estado.
    */
@@ -75,5 +98,37 @@ export class StatisticsService {
     const httpOptions = { headers };
 
     return this._http.get(this.url + 'average_waiting_time/', httpOptions);
+  }
+
+  /**
+   * Obtiene las estadísticas de citas por mes y por tipo para un año.
+   * @param year año
+   * @returns Observable con las estadísticas de citas por mes y por tipo para un año.
+   */
+  public getAppointmentsPerMonthAndType(year: number): Observable<any> {
+    const headers = this._httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    let params = new HttpParams();
+    params = params.append('year', year.toString());
+
+    return this._http.get(this.url + 'get_appointments_per_month_and_type/', {
+      params,
+      ...httpOptions,
+    });
+  }
+
+  /**
+   * Obtiene la cantidad de doctores por especialidad médica.
+   * @returns Observable con la cantidad de doctores por especialidad médica.
+   */
+  public getMedicalSpecialtyDoctorCount(): Observable<any> {
+    const headers = this._httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    return this._http.get(
+      this.url + 'medical_specialty_doctor_count/',
+      httpOptions
+    );
   }
 }
