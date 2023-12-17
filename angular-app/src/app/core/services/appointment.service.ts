93be35b90778b683a8e00c2a28c8ee60c08f8740
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-17 21:13:16 +0100

    Frontend - Añadido método para obtener citas de un día concreto de un paciente

--- angular-app/src/app/core/services/appointment.service.ts
+++ angular-app/src/app/core/services/appointment.service.ts
@@ -259,6 +259,75 @@ export class AppointmentService {
   }
 
   /**
+   * Obtiene las citas de un paciente en un día concreto.
+   * @returns Un observable que emite la respuesta del servidor.
+   */
+  public getAppointmentsByPatientAndDay(
+    date: string,
+    statuses: string[] | null,
+    page?: number,
+    numResults?: number,
+    searchTerm?: string,
+    paginated: boolean = false,
+    state?: boolean | null,
+    sortBy?: string,
+    sortOrder?: string
+  ): Observable<any> {
+    const headers = this._httpCommonService.getCommonHeaders();
+    const httpOptions = { headers };
+
+    let params = new HttpParams();
+
+    params = params.set('date', date);
+
+    if (statuses) {
+      // Si se han indicado los estados
+      statuses.forEach((status) => {
+        params = params.append('status', status);
+      });
+    }
+
+    if (paginated) {
+      // Si se quiere paginar
+
+      if (page) {
+        // Si se ha indicado la página
+        params = params.set('page', page.toString());
+      }
+
+      if (numResults) {
+        // Si se ha indicado el número de resultados por página
+        params = params.set('page_size', numResults.toString());
+      }
+
+      params = params.set('paginate', 'true'); // Indica que se quiere paginar
+    }
+
+    if (searchTerm) {
+      // Si se ha indicado un término de búsqueda
+      params = params.set('search', searchTerm);
+    }
+
+    if (state !== null && state !== undefined) {
+      // Si se ha indicado un estado
+      params = params.set('state', state.toString());
+    }
+
+    if (sortBy && sortOrder) {
+      // Si se ha indicado un campo por el que ordenar
+      params = params.set(
+        'ordering',
+        `${sortOrder === 'desc' ? '-' : ''}${sortBy}`
+      );
+    }
+
+    return this._http.get<any>(`${this.url}list_for_patient_by_date/`, {
+      params,
+      ...httpOptions,
+    });
+  }
+
+  /**
    * Actualiza el estado de una cita.
    * @param id ID de la cita.
    * @param status Estado de la cita.
