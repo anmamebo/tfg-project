72c7ff3d7898ba43c2ffb839cef0cd456ee916d2
Antonio Manuel <anmamebo2001@gmail.com>
2023-12-18 02:16:29 +0100

    Backend - Añadidos docstrings a serializador de horario

--- django-app/apps/schedules/api/serializers/schedule_serializer.py
+++ django-app/apps/schedules/api/serializers/schedule_serializer.py
@@ -4,12 +4,36 @@ from rest_framework import serializers
 
 
 class DoctorScheduleSerializer(serializers.ModelSerializer):
+    """
+    Serializador para representar los datos de un médico.
+
+    Este serializador maneja la representación de los datos de un médico,
+
+    Args:
+        validated_data (dict): Datos validados para la representación del médico.
+
+    Returns:
+        dict: Diccionario con los datos del médico.
+    """
+
     class Meta:
         model = Doctor
         fields = ["id", "user"]
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    """
+    Serializador para representar los datos de un horario.
+
+    Este serializador maneja la representación de los datos de un horario,
+
+    Args:
+        validated_data (dict): Datos validados para la representación del horario.
+
+    Returns:
+        dict: Diccionario con los datos del horario.
+    """
+
     doctor = DoctorScheduleSerializer(read_only=True)
 
     class Meta:
