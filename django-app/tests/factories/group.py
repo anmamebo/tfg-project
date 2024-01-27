b272464303d0a6f84161a120edcdf55cc356ae65
Antonio Manuel <anmamebo2001@gmail.com>
2024-01-27 19:08:55 +0100

    Backend - Implementada comprobación para que no se repita el nombre de un grupo

--- django-app/tests/factories/group.py
+++ django-app/tests/factories/group.py
@@ -14,3 +14,13 @@ class GroupFactory(factory.django.DjangoModelFactory):
         model = Group
 
     name = factory.Sequence(lambda n: f"{faker.name()} {n}")
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        name = kwargs.get("name", None)
+        if name:
+            existing_group = Group.objects.filter(name=name).first()
+            if existing_group:
+                return existing_group
+
+        return super()._create(model_class, *args, **kwargs)
