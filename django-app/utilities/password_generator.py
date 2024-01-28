23d18e382e427f2bed7b9f0a13739169d4ec7d78
Antonio Manuel <anmamebo2001@gmail.com>
2024-01-28 20:10:20 +0100

    Backend - Incrementada longitud generación contrraseña de 8 a 16

--- django-app/utilities/password_generator.py
+++ django-app/utilities/password_generator.py
@@ -2,6 +2,6 @@ import random
 import string
 
 
-def generate_password(length=8):
+def generate_password(length=16):
     characters = string.ascii_letters + string.digits + string.punctuation
     return "".join(random.choice(characters) for i in range(length))
