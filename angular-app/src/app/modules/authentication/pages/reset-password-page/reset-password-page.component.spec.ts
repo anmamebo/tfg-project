74306c6162974ddbe55a41593ee32e49bd8dde32
Antonio Manuel <anmamebo2001@gmail.com>
2024-01-29 18:50:28 +0100

    Frontend - Arreglado test reset-password-page component

--- angular-app/src/app/modules/authentication/pages/reset-password-page/reset-password-page.component.spec.ts
+++ angular-app/src/app/modules/authentication/pages/reset-password-page/reset-password-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 
 import { ResetPasswordPageComponent } from './reset-password-page.component';
 
@@ -8,9 +9,16 @@ describe('ResetPasswordPageComponent', () => {
   let fixture: ComponentFixture<ResetPasswordPageComponent>;
 
   beforeEach(() => {
+    const activatedRouteMock = {
+      snapshot: {
+        paramMap: convertToParamMap({ uid: 'user-id', token: 'sample-token' }),
+      },
+    };
+
     TestBed.configureTestingModule({
       declarations: [ResetPasswordPageComponent],
       imports: [HttpClientTestingModule],
+      providers: [{ provide: ActivatedRoute, useValue: activatedRouteMock }],
     });
     fixture = TestBed.createComponent(ResetPasswordPageComponent);
     component = fixture.componentInstance;
