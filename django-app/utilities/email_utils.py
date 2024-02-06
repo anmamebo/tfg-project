64c434ed368fbb897305b049dfc04d8af3351f0f
Antonio Manuel <anmamebo2001@gmail.com>
2024-02-06 17:06:16 +0100

    Backend - Refactorización email_utils

--- django-app/utilities/email_utils.py
+++ django-app/utilities/email_utils.py
@@ -3,23 +3,27 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 
-def send_welcome_email(user, password):
+def send_email(subject, template_name, recipient_list, context):
+    """
+    Envía un correo electrónico a la lista de destinatarios con el contenido del template_name.
+
+    Args:
+        subject (str): Asunto del correo electrónico.
+        template_name (str): Nombre del template que se usará para el contenido del correo electrónico.
+        recipient_list (list): Lista de destinatarios del correo electrónico.
+        context (dict): Diccionario con las variables que se usarán en el template.
+
+    Returns:
+        bool: True si el correo electrónico se envió correctamente, False en caso contrario.
+    """
     try:
-        email_content = render_to_string(
-            "emails/welcome_email_template.html",
-            {
-                "name": user.name,
-                "last_name": user.last_name,
-                "username": user.username,
-                "password": password,
-            },
-        )
+        email_content = render_to_string(f"emails/{template_name}.html", context)
 
         send_mail(
-            "Bienvenido/a a la aplicación de citas médicas HospitalSys",
+            subject,
             email_content,
             settings.EMAIL_HOST_USER,
-            [user.email],
+            recipient_list,
             html_message=email_content,
             fail_silently=False,
         )
@@ -29,54 +33,69 @@ def send_welcome_email(user, password):
     except Exception as e:
         print(f"Error al enviar el correo electrónico: {str(e)}")
         return False
+
+
+def send_welcome_email(user, password):
+    """
+    Envía un correo electrónico de bienvenida al usuario con su nombre de usuario y contraseña.
+
+    Args:
+        user (User): Usuario al que se le enviará el correo electrónico.
+        password (str): Contraseña del usuario.
+
+    Returns:
+        bool: True si el correo electrónico se envió correctamente, False en caso contrario.
+    """
+    return send_email(
+        "Bienvenido/a a la aplicación de citas médicas HospitalSys",
+        "welcome_email_template",
+        [user.email],
+        {
+            "name": user.name,
+            "last_name": user.last_name,
+            "username": user.username,
+            "password": password,
+        },
+    )
 
 
 def send_reset_password_email(user, url):
-    try:
-        email_content = render_to_string(
-            "emails/reset_password_email_template.html",
-            {
-                "name": user.name,
-                "url": url,
-            },
-        )
+    """
+    Envía un correo electrónico al usuario con el enlace para restablecer su contraseña.
 
-        send_mail(
-            "Restablecer contraseña HospitalSys",
-            email_content,
-            settings.EMAIL_HOST_USER,
-            [user.email],
-            html_message=email_content,
-            fail_silently=False,
-        )
+    Args:
+        user (User): Usuario al que se le enviará el correo electrónico.
+        url (str): Enlace que el usuario debe seguir para restablecer su contraseña.
 
-        return True
-
-    except Exception as e:
-        print(f"Error al enviar el correo electrónico: {str(e)}")
-        return False
+    Returns:
+        bool: True si el correo electrónico se envió correctamente, False en caso contrario.
+    """
+    return send_email(
+        "Restablecer contraseña HospitalSys",
+        "reset_password_email_template",
+        [user.email],
+        {
+            "name": user.name,
+            "url": url,
+        },
+    )
 
 
 def send_success_password_reset_email(user):
-    try:
-        email_content = render_to_string(
-            "emails/success_password_reset_email_template.html",
-            {
-                "name": user.name,
-            },
-        )
+    """
+    Envía un correo electrónico al usuario para notificarle que su contraseña se restableció correctamente.
 
-        send_mail(
-            "Contraseña restablecida correctamente HospitalSys",
-            email_content,
-            settings.EMAIL_HOST_USER,
-            [user.email],
-            html_message=email_content,
-            fail_silently=False,
-        )
+    Args:
+        user (User): Usuario al que se le enviará el correo electrónico.
 
-        return True
-
-    except Exception as e:
-        print(f"Error al enviar el correo electrónico: {str(e)}")
-        return False
+    Returns:
+        bool: True si el correo electrónico se envió correctamente, False en caso contrario.
+    """
+    return send_email(
+        "Contraseña restablecida correctamente HospitalSys",
+        "success_password_reset_email_template",
+        [user.email],
+        {
+            "name": user.name,
+        },
+    )
