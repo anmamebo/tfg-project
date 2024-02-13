e9609ea2b6d87e39ff34b97b505b5de8a22c05d8
Antonio Manuel <anmamebo2001@gmail.com>
2024-02-13 18:53:09 +0100

    Frontend - Solucionados tests componentes calendarios

--- angular-app/src/app/modules/appointments-patient/pages/appointments-calendar-patient-page/appointments-calendar-patient-page.component.spec.ts
+++ angular-app/src/app/modules/appointments-patient/pages/appointments-calendar-patient-page/appointments-calendar-patient-page.component.spec.ts
@@ -1,5 +1,14 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import localeEs from '@angular/common/locales/es';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  CalendarCommonModule,
+  CalendarDateFormatter,
+  DateAdapter,
+} from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CustomDateFormatter } from 'src/app/core/providers/custom-date-formatter.provider';
 import { AppointmentsCalendarPatientPageComponent } from './appointments-calendar-patient-page.component';
 
 describe('AppointmentsCalendarPatientPageComponent', () => {
@@ -7,9 +16,14 @@ describe('AppointmentsCalendarPatientPageComponent', () => {
   let fixture: ComponentFixture<AppointmentsCalendarPatientPageComponent>;
 
   beforeEach(() => {
+    registerLocaleData(localeEs);
     TestBed.configureTestingModule({
       declarations: [AppointmentsCalendarPatientPageComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, CalendarCommonModule],
+      providers: [
+        { provide: DateAdapter, useValue: adapterFactory },
+        { provide: CalendarDateFormatter, useClass: CustomDateFormatter },
+      ],
     });
     fixture = TestBed.createComponent(AppointmentsCalendarPatientPageComponent);
     component = fixture.componentInstance;
--- angular-app/src/app/modules/schedule/pages/schedule-page/schedule-page.component.spec.ts
+++ angular-app/src/app/modules/schedule/pages/schedule-page/schedule-page.component.spec.ts
@@ -1,5 +1,14 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import localeEs from '@angular/common/locales/es';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  CalendarCommonModule,
+  CalendarDateFormatter,
+  DateAdapter,
+} from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CustomDateFormatter } from 'src/app/core/providers/custom-date-formatter.provider';
 import { SchedulePageComponent } from './schedule-page.component';
 
 describe('SchedulePageComponent', () => {
@@ -7,9 +16,14 @@ describe('SchedulePageComponent', () => {
   let fixture: ComponentFixture<SchedulePageComponent>;
 
   beforeEach(() => {
+    registerLocaleData(localeEs);
     TestBed.configureTestingModule({
       declarations: [SchedulePageComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, CalendarCommonModule],
+      providers: [
+        { provide: DateAdapter, useValue: adapterFactory },
+        { provide: CalendarDateFormatter, useClass: CustomDateFormatter },
+      ],
     });
     fixture = TestBed.createComponent(SchedulePageComponent);
     component = fixture.componentInstance;
