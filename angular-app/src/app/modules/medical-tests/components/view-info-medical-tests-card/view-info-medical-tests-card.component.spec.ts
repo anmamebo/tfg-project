6174ba30943d98d1a7d28befbfafcc01b1dcb9e0
Antonio Manuel <anmamebo2001@gmail.com>
2024-02-13 17:54:16 +0100

    Frontend - Solucionado test

--- angular-app/src/app/modules/medical-tests/components/view-info-medical-tests-card/view-info-medical-tests-card.component.spec.ts
+++ angular-app/src/app/modules/medical-tests/components/view-info-medical-tests-card/view-info-medical-tests-card.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ViewInfoMedicalTestsCardComponent } from './view-info-medical-tests-card.component';
 
@@ -8,6 +9,7 @@ describe('ViewInfoMedicalTestsCardComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ViewInfoMedicalTestsCardComponent],
+      imports: [HttpClientTestingModule],
     });
     fixture = TestBed.createComponent(ViewInfoMedicalTestsCardComponent);
     component = fixture.componentInstance;
