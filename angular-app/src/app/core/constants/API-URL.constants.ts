bb70052b168b19909e169121ee1b14660b96b93c
Antonio Manuel <anmamebo2001@gmail.com>
2024-02-22 20:26:38 +0100

    Frontend - Cambiada url api

--- angular-app/src/app/core/constants/API-URL.constants.ts
+++ angular-app/src/app/core/constants/API-URL.constants.ts
@@ -1,3 +1,5 @@
-export const API_URL = 'http://localhost:8000/api/';
+// export const API_URL = 'http://localhost:8000/api/';
+export const API_URL = 'https://django-app.up.railway.app/api/';
 
-export const MEDIA_URL = 'http://localhost:8000';
+export const MEDIA_URL = 'https://django-app.up.railway.app/';
+// export const MEDIA_URL = 'http://localhost:8000';
