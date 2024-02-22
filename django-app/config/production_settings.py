5849d827fb39f9efd94ad20f3a5f50babc6e954b
Antonio Manuel <anmamebo2001@gmail.com>
2024-02-22 04:43:46 +0100

    Backend - Añadida carpeta static y actualizados settings

--- django-app/config/production_settings.py
+++ django-app/config/production_settings.py
@@ -206,7 +206,6 @@ SIMPLE_JWT = {
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
