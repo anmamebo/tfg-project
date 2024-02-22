9cc23837a60819e1be059be0b0125377bdc9ff6c
Antonio Manuel <anmamebo2001@gmail.com>
2024-02-22 04:30:01 +0100

    Backend - Actualizado settings

--- django-app/config/production_settings.py
+++ django-app/config/production_settings.py
@@ -204,7 +204,7 @@ SIMPLE_JWT = {
 
 # Configuración de archivos estáticos y multimedia
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
