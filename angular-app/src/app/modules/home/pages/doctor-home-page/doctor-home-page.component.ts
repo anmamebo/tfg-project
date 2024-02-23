11c188fd4926da5da0c377ec78bd229af6be6e01
Antonio Manuel <anmamebo2001@gmail.com>
2024-02-23 20:08:34 +0100

    Frontend - Actualizados estilos estadísticas

--- angular-app/src/app/modules/home/pages/doctor-home-page/doctor-home-page.component.ts
+++ angular-app/src/app/modules/home/pages/doctor-home-page/doctor-home-page.component.ts
@@ -3,5 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-doctor-home-page',
   templateUrl: './doctor-home-page.component.html',
+  styles: [
+    `
+      .today-calendar {
+        display: block;
+        height: 480px;
+      }
+    `,
+  ],
 })
 export class DoctorHomePageComponent {}
--- angular-app/src/app/modules/home/pages/patient-home-page/patient-home-page.component.ts
+++ angular-app/src/app/modules/home/pages/patient-home-page/patient-home-page.component.ts
@@ -3,5 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-patient-home-page',
   templateUrl: './patient-home-page.component.html',
+  styles: [
+    `
+      .today-calendar {
+        display: block;
+        height: 480px;
+      }
+    `,
+  ],
 })
 export class PatientHomePageComponent {}
