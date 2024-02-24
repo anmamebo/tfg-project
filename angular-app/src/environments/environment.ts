ff40f6c170aac9c2d2b7c9b033e51e0035b6ed5c
Antonio Manuel <anmamebo2001@gmail.com>
2024-02-24 22:32:45 +0100

    Frontend - Actualizada url archivos media para despliegue

--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://hospitalsys-api.up.railway.app/api/',
-  mediaUrl: 'https://hospitalsys-api.up.railway.app',
+  mediaUrl: 'https://storage.cloud.google.com/bucket_hospitalsys/',
 };
