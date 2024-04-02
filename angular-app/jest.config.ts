351e781fb982f4e7128d1658b1581e88b5d670c0
Antonio Manuel <anmamebo2001@gmail.com>
2024-04-02 16:07:29 +0200

    Frontend - Añadido mapeador de nombre de módulo

--- angular-app/jest.config.ts
+++ angular-app/jest.config.ts
@@ -92,6 +92,7 @@ const config: Config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
