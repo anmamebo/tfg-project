c193b2bdc136bb53266614f5f19c474db2f310b1
Antonio Manuel <anmamebo2001@gmail.com>
2024-04-12 17:40:37 +0200

    Frontend - Actualizada url API

--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://hospitalsys-api.up.railway.app/api/',
-  apiUrl: 'https://tfg-project-production.up.railway.app/api/',
+  apiUrl: 'https://hospitalsys-api.up.railway.app/api/',
   mediaUrl: '',
 };
