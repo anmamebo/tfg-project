1d7356d07581c8cb1da13c698279732a7d87bbc6
Antonio Manuel <anmamebo2001@gmail.com>
2024-04-12 16:08:05 +0200

    Frontend - Cambiada url api para probar nuevo despliegue

--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hospitalsys-api.up.railway.app/api/',
+  // apiUrl: 'https://hospitalsys-api.up.railway.app/api/',
+  apiUrl: 'https://tfg-project-production.up.railway.app/api/',
   mediaUrl: '',
 };
